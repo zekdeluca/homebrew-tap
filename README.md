3d07d7e472360cd194f5151e380609e7c3787b53
zekdeluca <ezequiel_deluca@yahoo.com>
2019-08-03 22:44:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-tap
+
+zekdeluca's personal homebrew tap
+
+## itermocil
+
+To install forked version of itermocil:
+
+`brew install zekdeluca/tap/itermocil`
---
+++ itermocil.rb
@@ -0,0 +1,33 @@
+class Itermocil < Formula
+  desc "iTermocil allows you setup pre-configured layouts of panes in iTerm2."
+  homepage "https://github.com/zekdeluca/itermocil"
+  url "https://github.com/zekdeluca/itermocil/archive/0.2.1.tar.gz"
+  sha256 "7ccd3f5e9f9a9654a71c55618fc4a77e04c0856a78d65f7741f5703ccaf832ef"
+
+  resource "PyYAML" do
+    url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
+    sha256 "c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8"
+  end
+
+  def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+
+    %w[PyYAML].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install "itermocil"
+    bin.install "itermocil.py"
+
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    system "#{bin}/itermocil", "-h"
+  end
+end
